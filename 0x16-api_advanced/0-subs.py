16c745685332237afaa5c848c7d9b9dc373f7fd5
AbdlouakilAlaoui <abdlouakilalaoui11@gmail.com>
2024-05-07 07:47:37 -0700

    abdo

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,17 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API for the number of subscribers of a subreddit.
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
 
-    Args:
-    subreddit (str): The subreddit to query.
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
 
-    Returns:
-    int: The number of subscribers if valid, 0 otherwise.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'subreddit_subscriber_counter/0.1'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check for a valid response and that we're not redirected
-    if response.status_code == 200 and response.history == []:
-        return response.json().get('data', {}).get('subscribers', 0)
-    return 0
+if __name__ == '__main__':
+    subreddit = input("Enter the subreddit name: ")
+    print(number_of_subscribers(subreddit))
